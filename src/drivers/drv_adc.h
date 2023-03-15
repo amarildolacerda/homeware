b615a5129691eb579d6149db343c2bd1fb9c44c1
Amarildo de Lacerda <amarildo_51@msn.com>
2023-03-15 20:13:24 -0300

    + min/max para adc quando estiver fora da faixa.

--- src/drivers/drv_adc.h
+++ src/drivers/drv_adc.h
@@ -6,17 +6,28 @@ class ADCDriver : public Driver
 {
 private:
     size_t interval = 500;
+    int eventState = 0;
+
     long ultimoLoop = 0;
     const String adc_interval = "adc_interval";
+    const String sMin = "adc_min";
+    const String sMax = "adc_max";
+    int tmpAdc = 0;
+    int min = 0;
+    int max = 0;
 
 public:
     void setup() override
     {
         Driver::setMode("adc");
         Driver::setup();
-        Protocol *p = getProtocol();
-        if (p->containsKey(adc_interval))
-            interval = p->getKey(adc_interval).toInt();
+        Protocol *prot = getProtocol();
+        if (prot->containsKey(adc_interval))
+            interval = prot->getKey(adc_interval).toInt();
+        if (prot->containsKey(sMin))
+            min = prot->getKey(sMin).toInt();
+        if (prot->containsKey(sMax))
+            max = prot->getKey(sMax).toInt();
         triggerEnabled = true;
     }
     void setPinMode(int pin) override
@@ -27,7 +38,10 @@ public:
     int readPin(const int pin) override
     {
         Driver::setPin(pin);
-        return analogRead(pin);
+        getAdcState(pin);
+        if (min == 0 && max == 0)
+            return tmpAdc;
+        return genStatus();
     }
     int writePin(const int pin, const int value) override
     {
@@ -36,13 +50,34 @@ public:
     }
     bool isGet() override { return true; }
     bool isSet() override { return true; }
-    bool isLoop() override { return active;}
-    void loop()override{
-        int pin = getPin();
-        if (millis() - ultimoLoop > interval && pin > -1)
+    bool isLoop() override { return active; }
+    void loop() override
+    {
+        if (eventState != genStatus())
         {
-            ultimoLoop = millis();
-            triggerCallback(getMode(), pin, readPin(pin));
+            eventState = genStatus();
+            triggerCallback(getMode(), getPin(), eventState);
         }
+    }
+    int genStatus()
+    {
+        int rt = eventState;
+        if (max > 0 && tmpAdc >= max)
+            rt = HIGH;
+        else if (min > 0 && tmpAdc < min)
+            rt = HIGH;
+        else
+            rt = LOW;
+        return rt;
+    }
+    int getAdcState(int pin)
+    {
+        if (millis() - ultimoLoop > interval)
+        {
+            Driver::setPin(pin);
+            tmpAdc = analogRead(pin);
+            ultimoLoop = millis();
+        }
+        return genStatus();
     }
 };
