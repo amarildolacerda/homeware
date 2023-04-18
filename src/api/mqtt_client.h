b7ad135a7d549a412e9c340af41f2f070c5ca7d1
Amarildo de Lacerda <amarildo_51@msn.com>
2023-04-17 22:25:16 -0300

    + usar interval

--- src/api/mqtt_client.h
+++ src/api/mqtt_client.h
@@ -12,9 +12,9 @@ protected:
     String password;
     String prefix;
     String name;
-    unsigned long lastOne = 0;
+    long lastOne = millis()-55000;
     unsigned long lastAlive = 0;
-    unsigned long interval = 10000;
+    unsigned long interval = 60000;
 
     void init();
 
