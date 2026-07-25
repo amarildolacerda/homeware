69d53d7e67a0e88cf1d9b5d0688547b21ca73073
kzuca <kzuca@local>
2026-07-25 11:30:22 -0300

    feat: add lora_config.h for SX1278 pinos e paramêtros

---
+++ hub/include/lora_config.h
@@ -0,0 +1,16 @@
+#ifndef LORA_CONFIG_H
+#define LORA_CONFIG_H
+
+#define LORA_SS_PIN     5
+#define LORA_RST_PIN    14
+#define LORA_DIO0_PIN   2
+#define LORA_DIO1_PIN   15
+
+#define LORA_FREQ       868.0
+#define LORA_SF         10
+#define LORA_BW         125
+#define LORA_CR         7
+#define LORA_PREAMBLE   8
+#define LORA_TX_POWER   17
+
+#endif
